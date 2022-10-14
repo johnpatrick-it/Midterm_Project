b2f3b800cd56b2e707f697ee0734688ccb2d78e8
johnpatrick-it <johnpatrickhaguimit@gmail.com>
2022-10-14 17:23:49 +0800

    Add project files.

---
+++ Midterm_Project/Controllers/CalcController.cs
@@ -0,0 +1,45 @@
+﻿using Midterm_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Midterm_Project.Controllers
+{
+    public class CalcController : Controller
+    {
+        //By John Patrick Haguimit SBIT3Q
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+
+        [HttpPost]
+
+        //Dito yung mga math 
+        public ActionResult Index(calc c,string calculate)
+        {
+            if(calculate == "circle") 
+            {
+                
+                c.total = Math.PI * c.no1 * c.no1;
+            }
+            else if (calculate == "triangle")
+            {
+                c.triangleTotal = (c.triangle1 * c.triangle2) / 2;
+            }
+            else if (calculate == "square")
+            {
+                c.squareTotal = c.square1 * c.square1;
+            }
+            else
+            {
+                c.rectangleTotal = c.rectangle1 * c.rectangle2;
+            }
+            return View(c);
+        }   
+
+    }
+}
---
+++ Midterm_Project/Models/calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Midterm_Project.Models
+{
+    public class calc
+    {
+        //By John Patrick Haguimit SBIT3Q
+        //circles
+        public double no1 { get; set; }
+        public double total { get; set; }
+        //triangles
+        public double triangle1 { get; set; }
+        public double triangle2 { get; set; }
+        public double triangleTotal { get; set; }
+        //squares
+        public double square1 { get; set; }
+        public double squareTotal { get; set; }
+        //rectangle
+        public double rectangle1 { get; set; }
+        public double rectangle2 { get; set; }
+        public double rectangleTotal { get; set; }
+
+
+
+    }
+}
